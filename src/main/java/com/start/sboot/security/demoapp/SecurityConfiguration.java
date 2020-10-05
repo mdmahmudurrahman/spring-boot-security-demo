ec4f9094c01b59046d8727d2d63733d27f080caf
mdmahmudurrahman <mailtommrr@gmail.com>
2020-10-05 13:36:23 +0200

    Customize Spring Security login form

--- src/main/java/com/start/sboot/security/demoapp/SecurityConfiguration.java
+++ src/main/java/com/start/sboot/security/demoapp/SecurityConfiguration.java
@@ -1,11 +1,15 @@
 package com.start.sboot.security.demoapp;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
+@EnableWebSecurity
 public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 	@Override
@@ -21,4 +25,19 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
 		// TODO Auto-generated method stub
 		return NoOpPasswordEncoder.getInstance();
 	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+			.antMatchers("/resources/**", "/static/**", "/css/**").permitAll()
+		    .anyRequest()
+		    .authenticated()
+		    .and()
+		    .formLogin()
+		    .loginPage("/login")
+		    .loginProcessingUrl("/authenticateUser")
+		    .permitAll();
+	}
+	
+	
 }
