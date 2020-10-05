d0fbcef65f6409efa8eceb763e287b3b7194127c
mdmahmudurrahman <mailtommrr@gmail.com>
2020-10-05 10:21:46 +0200

    Configure authentication in Spring Security

---
+++ src/main/java/com/start/sboot/security/demoapp/SecurityConfiguration.java
@@ -0,0 +1,24 @@
+package com.start.sboot.security.demoapp;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.inMemoryAuthentication()
+		    .withUser("boot")
+		    .password("1111")
+		    .roles("USER");
+	}
+	
+	@Bean
+	public PasswordEncoder getPasswordEncoder() {
+		// TODO Auto-generated method stub
+		return NoOpPasswordEncoder.getInstance();
+	}
+}
